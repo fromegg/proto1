20b6d5744bf649b7155dcd6dde42afc462ab9d23
fromegg <fromegg@yahoo.com>
2013-02-08 01:01:11 +0200

    -update

---
+++ TESTstory/GraphVC.h
@@ -0,0 +1,13 @@
+//
+//  GraphVC.h
+//  TESTstory
+//
+//  Created by igor on 2/7/13.
+//  Copyright (c) 2013 igor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GraphVC : UIViewController
+
+@end
---
+++ TESTstory/GraphVC.mm
@@ -0,0 +1,38 @@
+//
+//  GraphVC.m
+//  TESTstory
+//
+//  Created by igor on 2/7/13.
+//  Copyright (c) 2013 igor. All rights reserved.
+//
+
+#import "GraphVC.h"
+
+@interface GraphVC ()
+
+@end
+
+@implementation GraphVC
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
