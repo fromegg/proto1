90b4f24caefcd8237c274444eb898d7ea80b0bdf
fromegg <fromegg@yahoo.com>
2013-02-12 12:33:20 +0200

    - final updates with ugly hacks

---
+++ TESTstory/PatientProfile.h
@@ -0,0 +1,13 @@
+//
+//  PatientProfile.h
+//  TESTstory
+//
+//  Created by igor on 2/12/13.
+//  Copyright (c) 2013 igor. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PatientProfile : NSObject
+
+@end
---
+++ TESTstory/PatientProfile.m
@@ -0,0 +1,13 @@
+//
+//  PatientProfile.m
+//  TESTstory
+//
+//  Created by igor on 2/12/13.
+//  Copyright (c) 2013 igor. All rights reserved.
+//
+
+#import "PatientProfile.h"
+
+@implementation PatientProfile
+
+@end
